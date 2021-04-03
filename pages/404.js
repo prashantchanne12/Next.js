2213c5e244a3f41b0144209d9261e42b188ec283
prashantchanne12 <prashantchanne121@gmail.com>
2021-04-03 13:05:34 +0530

    rediercting

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,19 @@
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            // router.go(-1) -> just like pressing back button
+            // router.go(1) -> forward button
+            router.push('/');
+        }, 3000);
+    }, []);
+
     return (
         <div className='not-found'>
             <h1>Oooops....</h1>
