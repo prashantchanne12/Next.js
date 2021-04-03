fe682b3f2f589f5ed9066aefba73366c41c0cc1a
prashantchanne12 <prashantchanne121@gmail.com>
2021-04-03 11:17:04 +0530

    adding resusable page components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Navbar />
+      <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+      <Footer />
     </div>
   )
 }
