2174908d7e82ea33030b92bda3737e0d05ea0d58
prashantchanne12 <prashantchanne121@gmail.com>
2021-04-03 11:04:39 +0530

    setting up nextjs project and adding routes

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+        </div>
+     );
+}
+ 
+export default About;
